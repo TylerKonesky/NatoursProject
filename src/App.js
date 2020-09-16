ee83aedad7a0cfa9e3d857589a2d3f8d9a6ae0c8
TylerKonesky <tylerkonesky@hotmail.com>
2020-09-16 12:05:07 -0600

    grid setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from './Components/header';
+import Grids from './Components/gridStuff'
 import './css/style.css';
 
 function App() {
   return (
     <div>
         <Header />
+        <Grids />
     </div>
   );
 }
