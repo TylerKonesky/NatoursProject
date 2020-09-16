9d2912d6b2ddfd5f07f1297f7a53192f14658b04
TylerKonesky <tylerkonesky@hotmail.com>
2020-09-16 13:25:41 -0600

    about setup

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import './App.css';
 import Header from './Components/header';
 import Grids from './Components/gridStuff'
+import About from './Components/about'
 import './css/style.css';
 
 function App() {
   return (
     <div>
         <Header />
-        <Grids />
+        {/* <Grids /> */}
+        <About />
     </div>
   );
 }
